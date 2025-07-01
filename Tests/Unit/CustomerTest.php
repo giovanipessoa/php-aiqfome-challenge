2fbcfa36d0a2e215c0e06afd64df226e7526c801
DEXS <contato.dexs@gmail.com>
2025-06-30 22:25:08 -0300

    feat: add unit test for customer

---
+++ Tests/Unit/CustomerTest.php
@@ -0,0 +1,57 @@
+<?php
+
+// composer test:unit
+
+namespace Tests\Unit;
+
+use Domain\Entities\Customer;
+use PHPUnit\Framework\TestCase;
+use Domain\Enums\Commons\ValidationMessages;
+
+class CustomerTest extends TestCase
+{
+    public function testCreateCustomerWithValidData()
+    {
+        $name = 'Giovani Pessoa';
+        $email = 'giovanipessoa@live.com';
+
+        $customer = new Customer($name, $email);
+
+        $this->assertInstanceOf(Customer::class, $customer);
+        $this->assertEquals($name, $customer->name);
+        $this->assertEquals($email, $customer->email);
+    }
+
+    public function testCreateCustomerWithEmptyName()
+    {
+        $name = '';
+        $email = 'giovanipessoa@live.com';
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage(ValidationMessages::NAME_REQUIRED->value);
+
+        new Customer($name, $email);
+    }
+
+    public function testCreateCustomerWithEmptyEmail()
+    {
+        $name = 'Giovani Pessoa';
+        $email = '';
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage(ValidationMessages::EMAIL_REQUIRED->value);
+
+        new Customer($name, $email);
+    }
+
+    public function testCreateCustomerWithInvalidEmail()
+    {
+        $name = 'Giovani Pessoa';
+        $email = 'giovanipessoa';
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage(ValidationMessages::EMAIL_INVALID_FORMAT->value);
+
+        new Customer($name, $email);
+    }
+}
