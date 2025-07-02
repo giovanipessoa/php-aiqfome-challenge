f5105e6ce905b721140d50fc1c5c5fbc0dac9d69
DEXS <contato.dexs@gmail.com>
2025-07-02 11:42:54 -0300

    feat: add unit test for favorite product

---
+++ Tests/Unit/FavoriteProductTest.php
@@ -0,0 +1,46 @@
+<?php
+
+// composer test:unit
+
+namespace Tests\Unit;
+
+use Domain\Entities\FavoriteProduct;
+use PHPUnit\Framework\TestCase;
+use Domain\Enums\Commons\ValidationMessages;
+
+class FavoriteProductTest extends TestCase
+{
+    public function testCreateFavoriteProductWithValidData()
+    {
+        $customerId = 1;
+        $productId = 1;
+
+        $favoriteProduct = new FavoriteProduct($customerId, $productId);
+
+        $this->assertInstanceOf(FavoriteProduct::class, $favoriteProduct);
+        $this->assertEquals($customerId, $favoriteProduct->customerId);
+        $this->assertEquals($productId, $favoriteProduct->productId);
+    }
+
+    public function testCreateFavoriteProductWithEmptyCustomerId()
+    {
+        $customerId = '';
+        $productId = 1;
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage(ValidationMessages::CUSTOMER_ID_REQUIRED->value);
+
+        new FavoriteProduct($customerId, $productId);
+    }
+
+    public function testCreateFavoriteProductWithEmptyProductId()
+    {
+        $customerId = 1;
+        $productId = '';
+
+        $this->expectException(\Exception::class);
+        $this->expectExceptionMessage(ValidationMessages::PRODUCT_ID_REQUIRED->value);
+
+        new FavoriteProduct($customerId, $productId);
+    }
+}
