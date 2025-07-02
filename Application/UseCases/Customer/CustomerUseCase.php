df2fda5573ace10d63f48c419cb752f2872da892
DEXS <contato.dexs@gmail.com>
2025-07-02 12:28:14 -0300

    refactor: return customer after create

--- Application/UseCases/Customer/CustomerUseCase.php
+++ Application/UseCases/Customer/CustomerUseCase.php
@@ -4,6 +4,7 @@ namespace Application\UseCases\Customer;
 
 use Application\Interfaces\Customer\ICustomerRepository;
 use Domain\Entities\Customer;
+use Domain\Enums\Commons\ValidationMessages;
 
 class CustomerUseCase
 {
@@ -14,14 +15,24 @@ class CustomerUseCase
         $this->interface = $customerRepository;
     }
 
-    public function create(Customer $customer): void
+    public function create(Customer $customer): Customer
     {
+        if ($this->interface->getByEmail($customer->email)) {
+            throw new \Exception(ValidationMessages::EMAIL_ALREADY_EXISTS->value);
+        }
+
         $this->interface->create($customer);
+        return $customer;
     }
 
     public function getById(string $id): Customer
     {
         return $this->interface->getById($id);
+    }
+
+    public function getByEmail(string $email): Customer
+    {
+        return $this->interface->getByEmail($email);
     }
 
     public function update(Customer $customer): void
