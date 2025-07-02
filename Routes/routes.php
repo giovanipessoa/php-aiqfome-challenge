0d3678af81ca120b618921590d9339b70c31e937
DEXS <contato.dexs@gmail.com>
2025-07-02 20:10:28 -0300

    refactor: define tweaks to get only the id when the GET method is used for details

--- Routes/routes.php
+++ Routes/routes.php
@@ -3,13 +3,23 @@
 use WebUI\Controllers\CustomerController;
 
 $basePath = '/learning/php-aiqfome-challenge/Public';
-$requestUri = str_replace($basePath, '', $_SERVER['REQUEST_URI']);
 $method = $_SERVER['REQUEST_METHOD'];
+$uri = str_replace($basePath, '', $_SERVER['REQUEST_URI']);
+$route = "$method $uri";
 
-switch ("$method $requestUri") {
-    case 'POST /customer/create':
+if ($method === 'GET' && preg_match('#^/customer/(\d+)$#', $uri, $matches)) {
+    $id = (int) $matches[1];
+    $route = 'GET /customer/{id}';
+}
+
+switch ($route) {
+    case 'POST /customer':
         $controller = $container->get(CustomerController::class);
         $controller->create();
+        break;
+    case 'GET /customer/{id}':
+        $controller = $container->get(CustomerController::class);
+        $controller->getById($id);
         break;
     default:
         http_response_code(404);
