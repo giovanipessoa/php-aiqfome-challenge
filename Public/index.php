126277702287ee466a69afcdc5e2915cb2c0a143
DEXS <contato.dexs@gmail.com>
2025-07-02 16:46:47 -0300

    refactor: separate entry from routes

--- Public/index.php
+++ Public/index.php
@@ -2,15 +2,7 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-use WebUI\Controllers\CustomerController;
+// global handler
+header('Content-Type: application/json');
 
-$basePath = '/learning/php-aiqfome-challenge/Public';
-$requestUri = str_replace($basePath, '', $_SERVER['REQUEST_URI']);
-$method = $_SERVER['REQUEST_METHOD'];
-
-if ($requestUri == '/customer/create' && $method == 'POST') {
-    (new CustomerController())->create();
-} else {
-    http_response_code(404);
-    echo json_encode(['error' => 'Rota não encontrada']);
-}
+require_once __DIR__ . '/../Routes/routes.php';
