56b838d10879b8d11eec3b35a3034366714c7206
DEXS <contato.dexs@gmail.com>
2025-07-02 12:03:21 -0300

    feat: add application interface to the favorite product

---
+++ Application/Interfaces/IFavoriteProductRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Application\Interfaces;
+
+use Domain\Entities\FavoriteProduct;
+
+interface IFavoriteProductRepository
+{
+    public function create(FavoriteProduct $favoriteProduct): void;
+    public function getById(string $id): FavoriteProduct;
+    public function getByCustomerId(string $customerId): array;
+    public function delete(string $id): void;
+}
