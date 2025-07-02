7e965b28c87661ea6f95307f66ae2ee9ac2871ee
DEXS <contato.dexs@gmail.com>
2025-07-02 16:46:40 -0300

    feat: add routes file

---
+++ Routes/routes.php
@@ -0,0 +1,17 @@
+<?php
+
+use WebUI\Controllers\CustomerController;
+
+$basePath = '/learning/php-aiqfome-challenge/Public';
+$requestUri = str_replace($basePath, '', $_SERVER['REQUEST_URI']);
+$method = $_SERVER['REQUEST_METHOD'];
+
+switch ("$method $requestUri") {
+    case '/customer/create':
+        (new CustomerController())->create();
+        break;
+    default:
+        http_response_code(404);
+        echo json_encode(['error' => 'Rota não encontrada']);
+        break;
+}
