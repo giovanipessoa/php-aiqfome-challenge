bb15bd5d3cfd19d03bc1317c06358caa06892a33
DEXS <contato.dexs@gmail.com>
2025-07-02 19:46:51 -0300

    add: define container to inject dependency

--- Public/index.php
+++ Public/index.php
@@ -2,7 +2,13 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
+use DI\ContainerBuilder;
+
 // global handler
 header('Content-Type: application/json');
+
+$containerBuilder = new ContainerBuilder;
+$containerBuilder->addDefinitions(__DIR__ . '/../Infra/IoC/DependencyInjection.php');
+$container = $containerBuilder->build();
 
 require_once __DIR__ . '/../Routes/routes.php';
