c0516b8799ff6df75b68d642c3a6f37ff5f44f2b
DEXS <contato.dexs@gmail.com>
2025-07-02 15:29:26 -0300

    feat: add database config

---
+++ Infra/Data/Config/database.php
@@ -0,0 +1,9 @@
+return [
+'driver' => 'mysql',
+'host' => 'localhost',
+'port' => '3306',
+'dbname' => 'aiqfome',
+'user' => 'aiqfome',
+'password' => 'AeMhGZhvU3OLYuG9z1aN',
+'charset' => 'utf8mb4'
+];
