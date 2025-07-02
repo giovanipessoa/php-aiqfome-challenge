c6c044a71520919c193c2ccdecb5503cb6b60105
DEXS <contato.dexs@gmail.com>
2025-07-02 19:15:27 -0300

    refactor: change to return array

--- Infra/Data/Config/database.php
+++ Infra/Data/Config/database.php
@@ -1,9 +1,11 @@
+<?php
+
 return [
-'driver' => 'mysql',
-'host' => 'aiqfome.crueq47447kf.us-east-2.rds.amazonaws.com',
-'port' => '3306',
-'dbname' => 'aiqfome_db',
-'user' => 'aiqfome',
-'password' => 'AeMhGZhvU3OLYuG9z1aN',
-'charset' => 'utf8mb4'
+    'driver'   => 'mysql',
+    'host'     => 'aiqfome.crueq47447kf.us-east-2.rds.amazonaws.com',
+    'port'     => '3306',
+    'dbname'   => 'aiqfome_db',
+    'user'     => 'aiqfome',
+    'password' => 'AeMhGZhvU3OLYuG9z1aN',
+    'charset'  => 'utf8mb4',
 ];
