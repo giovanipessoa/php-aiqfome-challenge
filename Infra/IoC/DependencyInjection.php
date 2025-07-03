dace38156633c4348f2c534dc2e3e6eba0a6681f
DEXS <contato.dexs@gmail.com>
2025-07-03 10:10:52 -0300

    feat: add dependency injection file for inversion

--- Infra/IoC/DependencyInjection.php
+++ Infra/IoC/DependencyInjection.php
@@ -2,6 +2,9 @@
 
 use Infra\Data\Interfaces\Database\IDatabase;
 use Infra\Data\Context\AppDataBaseContext;
+use Infra\Auth\Service;
+use WebUI\Controllers\AuthController;
+use Application\Services\AuthService;
 
 // customers
 use Application\Interfaces\ICustomerRepository;
@@ -9,6 +12,9 @@ use Infra\Data\Repositories\CustomerRepository;
 
 return [
     IDatabase::class => \DI\create(AppDataBaseContext::class),
+    Service::class => \DI\create(Service::class),
+    AuthController::class => \DI\create(AuthController::class)
+        ->constructor(\DI\get(AuthService::class)),
 
     ICustomerRepository::class => \DI\create(CustomerRepository::class)
         ->constructor(\DI\get(IDatabase::class)),
