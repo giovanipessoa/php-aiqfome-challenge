7d0ae322046a95d3cac963dd609ad314418dcef3
DEXS <contato.dexs@gmail.com>
2025-07-03 16:27:17 -0300

    feat: add new messages for favorite product validate

--- Domain/Enums/Commons/ValidationMessages.php
+++ Domain/Enums/Commons/ValidationMessages.php
@@ -4,19 +4,18 @@ namespace Domain\Enums\Commons;
 
 enum ValidationMessages: string
 {
-    // Customer validation messages
+    // customers and favorite products
     case NAME_REQUIRED = 'O nome é obrigatório';
     case EMAIL_REQUIRED = 'O e-mail é obrigatório';
     case EMAIL_INVALID_FORMAT = 'O formato do e-mail é inválido';
     case EMAIL_ALREADY_EXISTS = 'Este e-mail já está cadastrado';
     case PASSWORD_REQUIRED = 'A senha é obrigatória';
     case PASSWORD_WEAK = 'A senha deve conter pelo menos uma letra maiúscula, uma minúscula e um número';
-
-        // Favorite product validation messages
     case PRODUCT_ID_REQUIRED = 'O produto é obrigatório';
     case CUSTOMER_ID_REQUIRED = 'O cliente é obrigatório';
+    case PRODUCT_NOT_FOUND = 'Produto não encontrado';
+    case PRODUCT_ALREADY_IN_FAVORITES = 'Produto já está nos favoritos';
 
-    // Get all name validation messages
     public static function getNameMessages(): array
     {
         return [
@@ -24,7 +23,6 @@ enum ValidationMessages: string
         ];
     }
 
-    // Get all email validation messages
     public static function getEmailMessages(): array
     {
         return [
@@ -33,7 +31,6 @@ enum ValidationMessages: string
         ];
     }
 
-    // Get all email already exists validation messages
     public static function getEmailAlreadyExistsMessages(): array
     {
         return [
@@ -41,12 +38,25 @@ enum ValidationMessages: string
         ];
     }
 
-    // Get all favorite product validation messages
     public static function getFavoriteProductMessages(): array
     {
         return [
             self::PRODUCT_ID_REQUIRED,
             self::CUSTOMER_ID_REQUIRED
+        ];
+    }
+
+    public static function getProductNotFoundMessages(): array
+    {
+        return [
+            self::PRODUCT_NOT_FOUND
+        ];
+    }
+
+    public static function getProductAlreadyInFavoritesMessages(): array
+    {
+        return [
+            self::PRODUCT_ALREADY_IN_FAVORITES
         ];
     }
 }
