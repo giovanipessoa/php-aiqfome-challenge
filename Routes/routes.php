c31ad8c32d146dfd432cef35f67092d8919f66e9
DEXS <contato.dexs@gmail.com>
2025-07-03 16:03:07 -0300

    feat: add new routes to
    - get all customer (GET)
    - update a customer (PUT)
    - delete a customer (DELETE)

--- Routes/routes.php
+++ Routes/routes.php
@@ -1,25 +1,66 @@
 <?php
 
+use WebUI\Controllers\AuthController;
 use WebUI\Controllers\CustomerController;
+use WebUI\Middlewares\AuthMiddleware;
 
 $basePath = '/learning/php-aiqfome-challenge/Public';
 $method = $_SERVER['REQUEST_METHOD'];
 $uri = str_replace($basePath, '', $_SERVER['REQUEST_URI']);
 $route = "$method $uri";
 
-if ($method === 'GET' && preg_match('#^/customer/(\d+)$#', $uri, $matches)) {
+$methods = [
+    'GET',
+    'PUT',
+    'DELETE'
+];
+
+if (in_array($method, $methods) && preg_match('#^/customer/(\d+)$#', $uri, $matches)) {
     $id = (int) $matches[1];
-    $route = 'GET /customer/{id}';
+    $route = $method . ' /customer/{id}';
 }
 
+$middleware = new AuthMiddleware();
+
 switch ($route) {
+    case 'POST /auth/login':
+        $controller = $container->get(AuthController::class);
+        $controller->login();
+        break;
     case 'POST /customer':
+        // require auth
+        $middleware->handle();
+
         $controller = $container->get(CustomerController::class);
         $controller->create();
         break;
     case 'GET /customer/{id}':
+        // require auth
+        $middleware->handle();
+
         $controller = $container->get(CustomerController::class);
         $controller->getById($id);
+        break;
+    case 'GET /customers':
+        // require auth
+        $middleware->handle();
+
+        $controller = $container->get(CustomerController::class);
+        $controller->getAll();
+        break;
+    case 'PUT /customer/{id}':
+        // require auth
+        $middleware->handle();
+
+        $controller = $container->get(CustomerController::class);
+        $controller->update($id);
+        break;
+    case 'DELETE /customer/{id}':
+        // require auth
+        $middleware->handle();
+
+        $controller = $container->get(CustomerController::class);
+        $controller->delete($id);
         break;
     default:
         http_response_code(404);
