49fa413dbc717b5f7367bab20d7d3184e258a97d
DEXS <contato.dexs@gmail.com>
2025-07-04 13:13:10 -0300

    refactor: change type of function return

--- Infra/Data/Services/External/Api/ProductService.php
+++ Infra/Data/Services/External/Api/ProductService.php
@@ -6,16 +6,16 @@ use Application\Interfaces\IProductService;
 
 class ProductService implements IProductService
 {
-    public function exists(int $productId): bool
+    public function exists(int $productId): array
     {
         $response = file_get_contents('https://fakestoreapi.com/products/' . $productId);
 
         if ($response === false) {
-            return false;
+            return [];
         }
 
         $data = json_decode($response, true);
 
-        return isset($data['id']) && $data['id'] == $productId;
+        return $data;
     }
 }
