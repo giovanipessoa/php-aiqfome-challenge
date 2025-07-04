164d6948110eb6643e512f26417bd7d67f71d4c9
DEXS <contato.dexs@gmail.com>
2025-07-04 13:13:55 -0300

    feat: add new dependency in class constructor
    - IProductRepository

--- Application/UseCases/FavoriteProductUseCase.php
+++ Application/UseCases/FavoriteProductUseCase.php
@@ -4,19 +4,25 @@ namespace Application\UseCases;
 
 use Application\Interfaces\IFavoriteProductRepository;
 use Application\Interfaces\IProductService;
+use Application\Interfaces\IProductRepository;
 use Domain\Entities\FavoriteProduct;
+use Domain\Entities\Product;
+use Domain\Enums\Commons\ValidationMessages;
 
 class FavoriteProductUseCase
 {
     private IFavoriteProductRepository $iFavoriteProductRepository;
     private IProductService $iProductService;
+    private IProductRepository $iProductRepository;
 
     public function __construct(
         IFavoriteProductRepository $favoriteProductRepository,
-        IProductService $productService
+        IProductService $productService,
+        IProductRepository $productRepository
     ) {
         $this->iFavoriteProductRepository = $favoriteProductRepository;
         $this->iProductService = $productService;
+        $this->iProductRepository = $productRepository;
     }
 
     /*
@@ -27,16 +33,42 @@ class FavoriteProductUseCase
 
     public function create(FavoriteProduct $favoriteProduct): void
     {
-        if (!$this->iProductService->exists($favoriteProduct->getProductId())) {
+        $productData = $this->iProductService->exists($favoriteProduct->getProductId());
+
+        if (!$productData) {
             throw new \Exception('Produto não encontrado.');
         }
 
+        $apiProduct = new Product(
+            $productData['id'],
+            $productData['title'],
+            $productData['image'],
+            (float) $productData['price'],
+            false
+        );
+
+        $existingProduct = $this->iProductRepository->getById($apiProduct->getId());
+
+        if ($existingProduct) {
+            if (
+                $existingProduct->getTitle() !== $apiProduct->getTitle() ||
+                $existingProduct->getImage() !== $apiProduct->getImage() ||
+                $existingProduct->getPrice() !== $apiProduct->getPrice()
+            ) {
+                $apiProduct->setReview(true);
+                $this->iProductRepository->update($apiProduct);
+            }
+        } else {
+            $this->iProductRepository->create($apiProduct);
+        }
+
         if ($this->iFavoriteProductRepository->exists($favoriteProduct->getProductId(), $favoriteProduct->getCustomerId())) {
-            throw new \Exception('Produto já está nos favoritos.');
+            throw new \Exception(ValidationMessages::PRODUCT_ALREADY_IN_FAVORITES->value);
         }
 
         $this->iFavoriteProductRepository->create($favoriteProduct);
     }
+
 
     /*
     * check if favorite product exists
